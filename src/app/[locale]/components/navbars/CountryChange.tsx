542951be9dd88676372d25c0e797832e96a6e09d
unknown <janithmadu281@gmail.com>
2025-01-06 13:11:38 +0530

    Second Section Update Done

--- src/app/[locale]/components/navbars/CountryChange.tsx
+++ src/app/[locale]/components/navbars/CountryChange.tsx
@@ -6,6 +6,7 @@ import UKflag from "../../../../../public/uk.png";
 import Arab from "../../../../../public/arab.png";
 import Image from "next/image";
 import Link from "next/link";
+import Loading from "../../loading";
 
 function getCookie(name: string) {
   const value = `; ${document.cookie}`;
@@ -22,6 +23,7 @@ function CountryChange() {
     // Fetch the locale from the cookie or default to 'en'
     return (getCookie("NEXT_LOCALE") as "en" | "ar") || "en";
   });
+  const [loader,setloader]=useState(false)
 
   const languages = {
     en: { label: "English", flag: UKflag, nextLocale: "ar" },
@@ -30,6 +32,7 @@ function CountryChange() {
 
   // Update the language and URL when switching
   const handleLanguageSwitch = (newLocale: "en" | "ar") => {
+    setloader(true)
     const segments = pathname.split("/");
   
     // Update the locale in the URL path
@@ -49,18 +52,24 @@ function CountryChange() {
   
     // Navigate to the new URL
     router.push(`${newPath}?${searchParams.toString()}`);
+    setloader(false)
   };
 
   return (
+    <>
     <div className="flex items-center justify-between min-w-full md:min-w-0 md:space-x-5 md:justify-end rtl:gap-10 border-l pl-2">
       <button
         onClick={() => handleLanguageSwitch(languages[locale].nextLocale as "en" | "ar")}
         className="flex items-center gap-2 rtl:gap-2"
-      >
+        >
         <Image width={25} src={languages[locale].flag} alt={languages[locale].nextLocale} />
       </button>
       <Link href={`/${locale}/commercial?slug=all`}>Commercial</Link>
     </div>
+    {
+      loader && <Loading/>
+    }
+      </>
   );
 }
 
